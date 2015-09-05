64cdbb90c92e3f6c63b5ab62e1f7862ce5ae357d
Hill <hill@kimo.com>
2015-09-05 16:29:31 +0400

    move session to a plugin

--- src/dbs/session.php
+++ src/dbs/session.php
@@ -2,43 +2,6 @@
 namespace IdOfThings\dbs;
 
 class session extends \IdOfThings\BaseGuidDb
-              implements \SessionHandlerInterface
 {
     protected $groupKey='session';
-
-    public function __construct($db)
-    {
-        parent::__construct($db);
-        session_set_save_handler($this, true);
-        session_start();
-    }
-    
-
-    public function open( $save_path , $session_name )
-    {
-    }
-
-    public function close()
-    {
-    }
-
-    public function read( $session_id )
-    {
-        return $this[$session_id];
-    }
-
-    public function write($session_id , $session_data )
-    {
-        $this[$session_id] = $session_data;
-    }
-
-    public function destroy( $session_id )
-    {
-        unset($this[$session_id]);
-    }
-
-    public function gc( $maxlifetime )
-    {
-        return true;
-    }
 }
