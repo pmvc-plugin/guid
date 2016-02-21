87f02fde038c79a278ab1b70a54f5b43661c2c17
Hill <hill@kimo.com>
2016-02-21 19:36:21 +0000

    add private db support

--- guid.php
+++ guid.php
@@ -6,23 +6,44 @@ ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\guid';
 class guid extends \PMVC\PlugIn
 {
     private $dbs;
+
     public function init()
     {
         $guid = new BigIntGuid(); 
         $this->setDefaultAlias($guid);
         $this['GUID_DB'] = \PMVC\plug('get')->get('GUID_DB');
+        $this['private_db'] = 'guid_private_db';
+    }
+
+    private function _getPrivateDb($key)
+    {
+        if (\PMVC\exists($this['private_db'],'plugin')) {
+            $private = \PMVC\plug($this['private_db']); 
+            $this->dbs[$key] = $private->getDb($key);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function _getPublicDb($key)
+    {
+        $class =  __NAMESPACE__.'\dbs\\'.$key;
+        if (!class_exists($class)) {
+            return !trigger_error('guid db not found ['.$class.']');
+        }
+        $this->dbs[$key] = new $class(
+            $this->getStorage()
+        );
+        return true;
     }
 
     public function getDb($key)
     {
         if(empty($this->dbs[$key])){
-            $class =  __NAMESPACE__.'\dbs\\'.$key;
-            if (!class_exists($class)) {
-                return !trigger_error('guid db not found ['.$class.']');
+            if (!$this->_getPrivateDb($key)) {
+                $this->_getPublicDb($key);
             }
-            $this->dbs[$key] = new $class(
-                $this->getStorage()
-            );
         }
         return $this->dbs[$key];
     }
--- src/dbs/minions.php
+++ src/dbs/minions.php
@@ -4,43 +4,6 @@ class minions extends \IdOfThings\BaseGuidDb
 {
     protected $groupKey='minions';
 
-    private $keys = array(
-        'list'=>'list_%s_%s',
-        'item'=>'item_%s_%s',
-        'url'=>'data_url_%s'
-    );
-
-    function getUrlKey($url, $md5Only=false)
-    {
-       $urlMd5 = md5($url); 
-       if ($md5Only) {
-          return $urlMd5;
-       } else {
-          return $this->getUrlKeyByMd5($urlMd5);
-       }
-    }
-
-    function getUrlKeyByMd5($md5)
-    {
-       $dbKey = sprintf($this->keys['url'], $md5);
-       if (!isset($this[$dbKey])) {
-            $this[$dbKey] = $url; 
-       }
-       return $dbKey;
-    }
-
-
-    function getListKey($date, $urlMd5)
-    {
-       $dbKey = sprintf($this->keys['list'], $date, $urlMd5);
-       return $dbKey;
-    }
-
-    function getItemKey($date, $urlMd5)
-    {
-       $dbKey = sprintf($this->keys['item'], $date, $urlMd5);
-       return $dbKey;
-    }
 
     function getDate()
     {
