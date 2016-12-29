71e8f538d9ff08c39328b177d00f093c3fe7e8dd
Hill <hill@kimo.com>
2016-12-29 18:59:02 +0000

    improve global key and guid

--- src/dbs/manager.php
+++ src/dbs/manager.php
@@ -3,6 +3,9 @@ namespace IdOfThings\dbs;
 
 class manager extends \IdOfThings\BaseGuidDb
 {
+    private $_key;
+    private $_guid;
+
     public function addNewKey($key)
     {
         $plugGuid = \PMVC\plug('guid'); 
@@ -46,5 +49,45 @@ class manager extends \IdOfThings\BaseGuidDb
             unset($gloKey[$oldKey]);
             $gloKey[$newKey] = $guid;
         }
+    }
+
+    public function getGuid($key)
+    {
+        return $this->getKeyDb()[$key];
+    }
+
+    public function hasGuid($key)
+    {
+        $db = $this->getKeyDb();
+        return isset($db[$key]);
+    }
+
+    public function getKey($guid)
+    {
+        return $this->getGuidDb()[$guid];
+    }
+
+    public function hasKey($guid)
+    {
+        $db = $this->getGuidDb();
+        return isset($db[$guid]);
+    }
+
+    public function getKeyDb()
+    {
+        if (empty($this->_key)) {
+            $this->_key = \PMVC\plug('guid')->
+                getDb('GlobalKey');
+        }
+        return $this->_key;
+    }
+
+    public function getGuidDb()
+    {
+        if (empty($this->_guid)) {
+            $this->_guid = \PMVC\plug('guid')->
+                getDb('GlobalGuid');
+        }
+        return $this->_guid;
     }
 }
